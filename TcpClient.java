d5afcba2ac90349cda0e3ffb2b158be69d2d1808
Nikhil-L <nikhil89515@gmail.com>
2019-12-03 12:28:32 +0530

    Add the TcpClient program

---
+++ TcpClient.java
@@ -0,0 +1,41 @@
+
+
+import java.net.*;
+import java.io.*;
+
+public class TcpClient
+{
+	public static void main(String []args)
+	{
+		try
+		{
+			Socket client = new Socket("localhost", 1537);
+			BufferedReader k = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.println("Enter the file location ");
+			String filename = k.readLine();
+			DataOutputStream sendToserver = new DataOutputStream(client.getOutputStream());
+			sendToserver.writeBytes(filename + "\n");
+
+			BufferedReader i = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			String string = i.readLine();
+			if(string.equals("Yes\n"))
+			{
+				while((string = i.readLine()) != null)
+					System.out.println(string);
+			}
+			else
+			{
+				System.out.println("File not found");
+			}
+			k.close();
+			i.close();
+			client.close();
+			sendToserver.close();
+		}
+		catch(Exception e)
+		{
+
+		}
+	}
+}
