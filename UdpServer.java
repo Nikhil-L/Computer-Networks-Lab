974e8e8f1360afe22633b6dbc5abe2997f1fdf34
Nikhil-L <nikhil89515@gmail.com>
2019-12-07 18:55:45 +0530

    Add server functionality of sending multiple msges to client

--- UdpServer.java
+++ UdpServer.java
@@ -13,8 +13,9 @@ public class UdpServer
 			System.out.println("Enter the server message ");
 
 			Scanner ip = new Scanner(System.in);
-
-			while(true)
+			
+			int count = 10;
+			while(count > 0)
 			{
 				byte[] buffer = new byte[1000];
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
@@ -24,9 +25,12 @@ public class UdpServer
 				byte[] sendMessage = msg.getBytes();
 				DatagramPacket reply = new DatagramPacket(sendMessage, sendMessage.length, request.getAddress(), request.getPort());
 				server.send(reply);
-				server.close();
-				ip.close();
+
+
 			}
+
+			server.close();
+			ip.close();
 		}
 		catch(Exception e)
 		{
